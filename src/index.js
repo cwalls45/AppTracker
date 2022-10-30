f421377d4dad0372c9288a34f0a4f2e0895e1469
cwalls45 <cwalls45@gmail.com>
2022-10-30 09:49:06 -0400

    CHEM-008: wrap app in Provider

--- src/index.js
+++ src/index.js
@@ -5,4 +5,8 @@ import App from './App';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
-root.render(<App/>);
+root.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+);
--- src/redux/action-creators/chemicalApplicationActionCreators.ts
+++ src/redux/action-creators/chemicalApplicationActionCreators.ts
@@ -2,9 +2,9 @@ import { Dispatch } from 'redux';
 import { Action } from '../../types/Action';
 import { ChemicalApplicationActions } from "../action-types/chemicalApplicationActionTypes"
 
-export const addAreaOfApplication = (area: string) => {
+export const addAreaOfApplication = (addAreaOfApplication: string) => {
     return (dispatch: Dispatch<Action>) => dispatch({
         type: ChemicalApplicationActions.ADD_AREA_OF_APPLICATION,
-        payload: area
+        payload: addAreaOfApplication
     })
 }
