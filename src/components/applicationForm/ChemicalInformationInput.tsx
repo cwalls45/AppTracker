b0c888641fdd41893e6bccf244ef4bf07a34a318
cwalls45 <cwalls45@gmail.com>
2022-12-06 21:51:25 -0500

    CHEM-015: refactor chemicalSelect to remove as much logic out as possible

--- src/components/applicationForm/ChemicalInformationInput.tsx
+++ src/components/applicationForm/ChemicalInformationInput.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import Grid from '@mui/material/Grid';
 import ChemicalSelect from "./ChemicalSelect";
 import FormInputText from "./FormInputText";
@@ -6,6 +7,7 @@ import { ChemicalProperties } from "../../entities/chemicalApplicationFormDefaul
 import { searchChemicalNames } from "../../utils/apiRequests";
 import { State } from '../../redux';
 import { useSelector } from 'react-redux';
+import { useDebounce } from '../../hooks/useDebounce';
 
 interface IProps {
     index: number;
@@ -15,7 +17,39 @@ const ChemicalInformationInput = ({ index }: IProps) => {
 
     const units = ['lbs', 'oz', 'gallon(s)', 'fl. oz'];
 
+    const [options, setOptions] = useState([]);
+    const [isSearching, setIsSearching] = useState(false);
+    const [isDisabled, setIsDisabled] = useState(false);
     const { chemicalApplication } = useSelector((state: State) => state);
+    const debouncedChemicalName = useDebounce(chemicalApplication.chemicals[index].chemicalName, 300);
+
+    const fetchData = async (
+        searchValue: string,
+        apiRequestFunc: (queryString: string) => Promise<any>
+    ) => {
+        const searchResults = await apiRequestFunc(searchValue);
+        console.log('searchResults', searchResults)
+        setOptions(searchResults);
+        setIsSearching(false);
+    };
+
+    useEffect(() => {
+        console.log('in useeffect', debouncedChemicalName)
+        console.log('index', index)
+        if (debouncedChemicalName) {
+            console.log('inDebounceChemicalName', debouncedChemicalName)
+            setIsSearching(true);
+            fetchData(chemicalApplication.chemicals[index].chemicalName, searchChemicalNames)
+                .catch((error) => {
+                    setIsSearching(false);
+                    console.log('Error in ChemicalSelect: ', error);
+                })
+        } else {
+            setOptions([]);
+            setIsSearching(false);
+        }
+    }, [debouncedChemicalName]);
+
 
     return (
         <Grid container justifyContent='space-around'>
@@ -24,10 +58,9 @@ const ChemicalInformationInput = ({ index }: IProps) => {
                     index={index}
                     property={ChemicalProperties.CHEMICAL_NAME}
                     label='Chemical Name'
-                    defaultOptions={[]}
-                    dependentValue={null}
-                    apiRequestFunc={searchChemicalNames}
-
+                    options={options}
+                    isSearching={isSearching}
+                    isDisabled={false}
                 />
             </Grid>
             <Grid item xs={12} md={4}>
@@ -35,8 +68,9 @@ const ChemicalInformationInput = ({ index }: IProps) => {
                     index={index}
                     property={ChemicalProperties.CHEMICAL_COMPANY}
                     label='Chemical Company'
-                    defaultOptions={chemicalCompanyNames}
-                    dependentValue={chemicalApplication.chemicals[index].chemicalName}
+                    options={chemicalCompanyNames}
+                    isSearching={isSearching}
+                    isDisabled={false}
                 />
             </Grid>
             <Grid item xs={6} md={1}>
@@ -52,8 +86,9 @@ const ChemicalInformationInput = ({ index }: IProps) => {
                     index={index}
                     property={ChemicalProperties.UNITS}
                     label='Units'
-                    defaultOptions={units}
-                    dependentValue={null}
+                    options={units}
+                    isSearching={false}
+                    isDisabled={false}
                 />
             </Grid>
         </Grid >
