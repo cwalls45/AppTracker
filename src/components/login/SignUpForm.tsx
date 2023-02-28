c47c84d7a9eb8a0e527dad562a2dfd4e0c238692
cwalls45 <cwalls45@gmail.com>
2023-02-27 22:59:58 -0500

    CHEM-027: validate email and password

--- src/components/login/SignUpForm.tsx
+++ src/components/login/SignUpForm.tsx
@@ -8,51 +8,76 @@ const SignUpForm = () => {
     const [password, setPassword] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
 
+    const handleSubmit = () => {
+        const isEmailValid = validateEmail();
+        const { passwordValidations, passwordIsValidated } = validatePasswords();
+        console.log(isEmailValid, passwordValidations)
+    }
+
+    const validateEmail = () => {
+        const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+        ;
+        const isValidEmail = emailRegex.test(email);
+
+        return isValidEmail;
+    }
+
+    const validatePasswords = () => {
+        const matchPasswords = password === confirmPassword;
+        const containsUpperCase = /[A-Z]/.test(password);
+        const containsLowerCase = /[a-z]/.test(password);
+        const containsNumber = /[0-9]/.test(password);
+        const meetsMinimumLength = password.length >= 8;
+
+        const validations = [matchPasswords, containsUpperCase, containsLowerCase, containsNumber, meetsMinimumLength];
+        const isValidated = validations.every((isTrue) => isTrue);
+
+        return {
+            passwordValidations: validations,
+            passwordIsValidated: isValidated
+        }
+    }
+
     return (
-        <Grid container sx={{
-            height: '100vh',
-            width: 'auto'
-        }}>
-            <Grid container item justifyContent='center'>
+        <Grid container justifyContent='center' alignItems='center' sx={{ height: '100vh', width: 'auto' }}>
+            <Grid container justifyContent='center'>
                 <Typography variant="h4" component="div">
                     Create Username and Password
                 </Typography>
             </Grid>
-            <Grid container item xs={12} justifyContent='center' rowSpacing={3}>
-                <Grid item xs={6.5}>
-                    <FormTextField
-                        label='Email'
-                        value={email}
-                        setterFunction={setEmail}
-                    />
+            <Grid container justifyContent='center'>
+                <Grid container item xs={12} justifyContent='center' rowSpacing={3}>
+                    <Grid item xs={6.5}>
+                        <FormTextField
+                            label='Email'
+                            value={email}
+                            setterFunction={setEmail}
+                        />
+                    </Grid>
+                    <Grid item xs={6.5}>
+                        <FormTextField
+                            label='Password'
+                            value={password}
+                            setterFunction={setPassword}
+                            type='password'
+                        />
+                    </Grid>
+                    <Grid item xs={6.5}>
+                        <FormTextField
+                            label='Confirm Password'
+                            value={confirmPassword}
+                            setterFunction={setConfirmPassword}
+                            type='password'
+                        />
+                    </Grid>
                 </Grid>
-                <Grid item xs={6.5}>
-                    <FormTextField
-                        label='Password'
-                        value={password}
-                        setterFunction={setPassword}
-                        type='password'
-                    />
+                <Grid container item xs={12} justifyContent='center'>
+                    <Grid item>
+                        <Button onClick={handleSubmit} variant='contained' sx={{ flexGrow: 1, width: '15em' }}>
+                            Sign Up
+                        </Button>
+                    </Grid>
                 </Grid>
-                <Grid item xs={6.5}>
-                    <FormTextField
-                        label='Confirm Password'
-                        value={confirmPassword}
-                        setterFunction={setConfirmPassword}
-                        type='password'
-                    />
-                </Grid>
-            </Grid>
-            <Grid container item xs={12} justifyContent='center'>
-                {/* <Grid item>
-                    <Button variant='contained' onClick={handleSubmit} sx={{
-                        flexGrow: 1,
-                        width: '15em'
-                    }}
-                    >
-                        Sign In
-                    </Button>
-                </Grid> */}
             </Grid>
         </Grid >
     );
