7b753190b050c1bf68cc408c4aa071e7238370d0
cwalls45 <cwalls45@gmail.com>
2023-04-22 08:53:41 -0400

    CHEM-030: add reports route

---
+++ src/components/reports/Reports.tsx
@@ -0,0 +1,16 @@
+import { Grid, Typography } from "@mui/material";
+
+const Reports = () => {
+
+    return (
+        <Grid container sx={{ height: '100%', width: 'auto' }}>
+            <Grid container justifyContent='center'>
+                <Typography variant="h3" component="div">
+                    Reports
+                </Typography>
+            </Grid>
+        </Grid>
+    )
+};
+
+export default Reports;
