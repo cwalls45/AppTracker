504622cf5796fc0029d1dacbae50668333ef062a
cwalls45 <cwalls45@gmail.com>
2023-04-27 22:12:01 -0400

    CHEM-031: set applications to state

--- src/components/reports/Reports.tsx
+++ src/components/reports/Reports.tsx
@@ -1,11 +1,41 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Grid } from "@mui/material";
 import ReportTabs from "./ReportTabs";
 import TabDisplay from "./TabDisplay";
 import DataTable from "./DataTable";
+import { useDispatch, useSelector } from "react-redux";
+import { bindActionCreators } from "redux";
+import { State, applicationsActionCreators } from "../../redux";
+import { IApplication } from "../../entities/chemicalApplicationFormDefaultValues";
+import { getApplications } from "../../utils/applicationRequests";
 
 const Reports = () => {
     const [selectedTab, setSelectedTab] = useState(0);
+    const [year, setYear] = useState('2023');
+    const [chemicalsApplied, setChemicalsApplied] = useState<IApplication[]>([]);
+
+    const dispatch = useDispatch();
+    const { setAllApplications: getAllApplications } = bindActionCreators(applicationsActionCreators, dispatch);
+    const { applications } = useSelector((state: State) => state);
+
+    useEffect(() => {
+        console.log(1)
+        //TODO: create dropdown for year
+        getApplications(year).then((apps) => {
+            getAllApplications(apps);
+            console.log(2)
+        });
+        console.log(3)
+    }, []);
+
+    useEffect(() => {
+        const chemicals = applications.map((app) => {
+            return app.chemicals.map((chemical) => chemical)
+        }).flat();
+        console.log(chemicals)
+
+        // setChemicalsApplied(chemicals);
+    }, [applications]);
 
     return (
         <Grid container id='test' justifyContent='center' alignItems='flex-start' sx={{ height: '100vh', width: 'auto' }}>
--- src/redux/action-creators/applicationActionCreators.ts
+++ src/redux/action-creators/applicationActionCreators.ts
@@ -2,10 +2,17 @@ import { Dispatch } from "redux";
 import { IApplication } from "../../entities/chemicalApplicationFormDefaultValues";
 import { ApplicationActions } from "../actions/applicationsActions";
 import { ApplicationsActionsTypes } from "../../entities/applicationsActionTypes";
+import { getApplications } from "../../utils/applicationRequests";
+import { EnvironmentActionTypes } from "../../entities/environmentActionTypes";
+import { EnvironmentActions } from "../actions/environmentActions";
 
 export const setAllApplications = (applications: IApplication[]) => {
-    return (dispatch: Dispatch<ApplicationsActionsTypes>) => dispatch({
-        type: ApplicationActions.SET_ALL_APPLICATIONS,
-        payload: applications
-    });
+    return async (dispatch: Dispatch<ApplicationsActionsTypes>) => {
+
+        dispatch({
+            type: ApplicationActions.SET_ALL_APPLICATIONS,
+            payload: applications
+        });
+
+    }
 }
