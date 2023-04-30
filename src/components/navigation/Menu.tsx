25b9ac34297ef9937c3b65ab099d5934cc8ccccc
cwalls45 <cwalls45@gmail.com>
2023-04-30 19:38:29 -0400

    CHEM-032: remove tooltip from menu button

--- src/components/navigation/Menu.tsx
+++ src/components/navigation/Menu.tsx
@@ -20,51 +20,19 @@ const Menu = () => {
 
     return (
         <>
-            <Tooltip title="Main Menu">
-                <IconButton
-                    onClick={handleClick}
-                    size="small"
-                    sx={{ ml: 2 }}
-                    aria-controls={open ? 'Main Menu' : undefined}
-                    aria-haspopup="true"
-                    aria-expanded={open ? 'true' : undefined}
-                >
-                    <Avatar sx={{ width: 45, height: 45 }}>M</Avatar>
-                </IconButton>
-            </Tooltip>
+            <IconButton
+                onClick={handleClick}
+                size="medium"
+                sx={{ ml: 2 }}
+                aria-label='Main Menu Button'
+            >
+                <Avatar sx={{ width: 45, height: 45 }} variant="rounded">M</Avatar>
+            </IconButton>
             <MenuMUI
                 open={open}
                 onClick={handleClose}
                 onClose={handleClose}
                 anchorEl={anchorElement}
-                PaperProps={{
-                    elevation: 0,
-                    sx: {
-                        overflow: 'visible',
-                        filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
-                        mt: 1.5,
-                        '& .MuiAvatar-root': {
-                            width: 45,
-                            height: 45,
-                            ml: -0.5,
-                            mr: 1,
-                        },
-                        '&:before': {
-                            content: '""',
-                            display: 'block',
-                            position: 'absolute',
-                            top: 0,
-                            right: 14,
-                            width: 10,
-                            height: 10,
-                            bgcolor: 'background.paper',
-                            transform: 'translateY(-50%) rotate(45deg)',
-                            zIndex: 0,
-                        },
-                    },
-                }}
-                transformOrigin={{ horizontal: 'right', vertical: 'bottom' }}
-                anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
             >
                 <NavigationButtons clickFunction={handleMenuItemClick} />
             </MenuMUI>
